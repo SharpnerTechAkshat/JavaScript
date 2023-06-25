a05aa8b9c974496bd1b493c4fc28bdfe31acab06
SharpnerTechAkshat <137689820+SharpnerTechAkshat@users.noreply.github.com>
2023-06-26 00:34:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log(document.head)
+// console.log(document.images)
+// console.log(document.forms)
+
+//-------GETELEMENTBYID-------------//
+// const element = document.getElementById('items') 
+// console.log(element)
+
+//const element2 = document.getElementById('header-title') 
+const element2 = document.getElementById('main-header') 
+// console.log(element2)
+
+// element2.textContent = 'Sharpner'
+
+element2.style.border = 'solid 3px #000'
+
+const element3 = document.getElementById('h2')
+element3.style.color = 'green'
+element3.style.fontWeight = 'bold'
+
